9fdbfb6bc04f665f62d1c21f812f8eeff7c48b68
alexander Bous <alexmbous@gmail.com>
2018-02-01 12:00:23 -0600

    arrays manipulation section exercises and bonuses. also moved locations of items.

---
+++ js/prework-planets-array.js
@@ -0,0 +1,75 @@
+(function(){
+    "use strict";
+
+    //  Create an array holding the names of the eight planets in our solar system in order, starting closest to the sun.
+    var planets = [];
+
+    // function for logging the planets array
+    function logPlanets() {
+        console.log("Here is the list of planets:");
+        console.log(planets);
+        console.log("---- ---- ---- ----");
+    }
+
+    logPlanets();
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+    // Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    // Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+
+    console.log('Using splice to remove the planet after "Earth".');
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log('Using splice to add back the planet after "Earth".');
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log("Reversing the order of the planets array.");
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+    console.log("Sorting the planets array.");
+    //  Read the console.log() statement above. Write code to perform the step it describes.
+    logPlanets();
+
+
+    (function(){
+        "use strict";
+
+        var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+        var planetsArray;
+
+        //  Convert planetsString to an array, save it to planetsArray.
+
+        console.log(planetsArray);
+
+        //  Create a string with <br> tags between each planet. console.log() your results.
+        //       Why might this be useful?
+
+        // Bonus: Create a second string that would display your planets in an undordered list.
+        //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
+        //        console.log() your results.
+    })();
+
+
+
+
+
+
+})();
