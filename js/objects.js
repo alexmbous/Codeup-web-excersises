7990768824d12a6e26c3ad016b37871ae746678f
alexander Bous <alexmbous@gmail.com>
2018-02-02 12:03:54 -0600

    Math circle and Objects exercises.

--- js/objects.js
+++ js/objects.js
@@ -79,18 +79,6 @@
      * > console.log(books[0].author.lastName) // "Adams"
      */
 
-var books = [
-        {title : "Adventures of Tin Tin", author : [ "Herge", ""]},
-        {title : "The Jungle Book", author : ["Rudyard","Kipling"]},
-        {title : "Husk", author : ["Sean", "Brock"]}
-];
-    books.forEach(function(book) {
-            book.author.forEach(function() {
-                console.log("the title is: " + book.title + ". The Author is:  " + book.author[0], book.author[1]);
-            });
-        });
-
-
 
     /**
      * TODO:
@@ -116,6 +104,21 @@ var books = [
      *      ---
      *      ...
      */
+    var books = [
+        {title : "Adventures of Tin Tin", author : {firstName: "Herge", lastName: ""}},
+        {title : "The Jungle Book", author : {firstName: "Rudyard", lastName: "Kipling"}},
+        {title : "Husk", author : {firstName :"Sean", lastName:  "Brock"}}
+    ];
+    books.forEach(function(book, index) {
+        var output = "";
+        output += "Book # " + (index + 1) + "\n";
+        output += "Title: " + book.title + "\n";
+        output += "Author: " + book.author.firstName + " " + book.author.lastName + "\n";
+        output += "---\n";
+        console.log(output);
+
+    });
+
 
     /**
      * Bonus:
@@ -127,5 +130,69 @@ var books = [
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+    // var createBook = [title, authorFirstName, authorLastName];
+    // createBook.logNewBook = function(){
+    //     prompt("Enter Title and Author Name.").split(",");
+    // };
+    // console.log("Added Book Title: " + this.title + "Author Name : " + createBook.authorFirstName + createBook.authorLastName);
+
+
+    // createBook.title = (titleInput);
+    // createBook.authorFirstName = (authorFirstName);
+    // createBook.authorLastName = (authorLastName);
+
+
+
+    // BONUS 1 (create a dog object):
+    // The dog object should have properties for:
+    // breed (string),
+    //     weightInPounds (number),
+    //     age (number),
+    //     color (string),
+    //     sterilized (boolean),
+    //     shotRecords (array of objects with properties for date and typeOfShot)
+    // The dog object should have methods to:
+    //     bark() - will console.log "Woof!"
+    // getOlder() - will increase age by 1
+    // fix() - will set sterile to true if dog sterilized property is false
+    // vaccinate() - takes in an argument for the name of the shot and adds a new shot with the current date to the shotRecords array
+
+
+
+
+
+
+
+
+
+    // BONUS 2 (expanding on the books object exercise):
+    // Add a property "keywords" that contains an array of possible genres the book may be categorized by
+    // Add a boolean property "available" and set it to true
+    // Add a dateAvailable property that has a string of the date/time when the book will be available
+    // Add a method lend() that...
+    // - changes the available property to false if it is not already false
+    // - sets the dateAvailable to a date exactly two weeks from when the lend() method is called
+    // (to do this, research the JS Date object and use methods from it in your code)
+    // Add a method receive() that...
+    // - changes the available property to true
+    // - changes the dateAvailable property to the string "now"
+
+
+
+
+
+
+    // BONUS 3 (expanding on the books object exercise):
+    // Create an application to take in user input to build the books array of objects.
+    //     Allow the user to continue adding books or to finish adding books.
+    //     Once the books have been added, output the books array in the console.
+    //     Allow a user to delete a book or a group of books by title or author last name
+    // Allow a user to edit a book by index number in the books array
+
+
+
+
+
+
 
 })();
