dbee6d0e8afdada428a0619c79201360410292cc
alexander Bous <alexmbous@gmail.com>
2018-02-22 09:57:54 -0600

    refactoring old projects

--- js/Home_inventory_program.js
+++ js/Home_inventory_program.js
@@ -1,69 +1,100 @@
-const inventory = [
-    {
-        name: 'butter',
-        standard: ['1', 'lb'],
-        have: ['.5', 'lb'],
-        need: ['.5', 'lb'],
-        avgCost: ['$2.25', 'lb'],
-        category: 'Dairy'
-    },
-    {
-        name: 'coffee',
-        standard: ['1', 'lb'],
-        have: ['.5', 'lb'],
-        need: ['.5', 'lb'],
-        avgCost: ['$9', 'lb'],
-        category: 'Pantry'
-    },
-    {
-        name: 'Dog Food',
-        standard: ['1', 'bag'],
-        have: ['.25', 'bag'],
-        need: ['.75', 'bag'],
-        avgCost: ['$53.99', 'bag'],
-        category: 'Pantry'
-    },
-    {
-        name: 'Baby Formula',
-        standard: ['6', 'tubs'],
-        have: ['4', 'tubs'],
-        need: ['2', 'tubs'],
-        avgCost: ['$25', 'ea'],
-        category: 'Dairy'
-    },
-    {
-        name: 'paper towels',
-        standard: ['12', 'lb'],
-        have: ['.5', 'lb'],
-        need: ['.5', 'lb'],
-        avgCost: ['$10.25', 'pack'],
-        category: 'Dairy'
-    },
-    {
-        name: 'Olive Oil',
-        standard: ['1', 'gal'],
-        have: ['1', 'gal'],
-        need: ['0', 'gal'],
-        avgCost: ['$61.00', 'gal'],
-        category: 'Dairy'
-    },
-    {
-        name: 'Chicken',
-        standard: ['5', 'lb'],
-        have: ['5', 'lb'],
-        need: ['0', 'lb'],
-        avgCost: ['$4.95', 'lb'],
-        category: 'Dairy'
-    },
-    {
-        name: 'Topo Chico',
-        standard: ['1', 'case'],
-        have: ['.5', 'case'],
-        need: ['.5', 'case'],
-        avgCost: ['$23', 'case'],
-        category: 'Dairy'
-    },
-];
+(function() {
+    "use strict";
+
+    const inventory = [
+        {
+            name: 'butter',
+            standard: ['1', 'lb'],
+            have: ['.5', 'lb'],
+            need: ['.5', 'lb'],
+            avgCost: ['$2.25', 'lb'],
+            category: 'Dairy'
+        },
+        {
+            name: 'coffee',
+            standard: ['1', 'lb'],
+            have: ['.5', 'lb'],
+            need: ['.5', 'lb'],
+            avgCost: ['$9', 'lb'],
+            category: 'Pantry'
+        },
+        {
+            name: 'Dog Food',
+            standard: ['1', 'bag'],
+            have: ['.25', 'bag'],
+            need: ['.75', 'bag'],
+            avgCost: ['$53.99', 'bag'],
+            category: 'Pantry'
+        },
+        {
+            name: 'Baby Formula',
+            standard: ['6', 'tubs'],
+            have: ['4', 'tubs'],
+            need: ['2', 'tubs'],
+            avgCost: ['$25', 'ea'],
+            category: 'Dairy'
+        },
+        {
+            name: 'paper towels',
+            standard: ['12', 'lb'],
+            have: ['.5', 'lb'],
+            need: ['.5', 'lb'],
+            avgCost: ['$10.25', 'pack'],
+            category: 'Dairy'
+        },
+        {
+            name: 'Olive Oil',
+            standard: ['1', 'gal'],
+            have: ['1', 'gal'],
+            need: ['0', 'gal'],
+            avgCost: ['$61.00', 'gal'],
+            category: 'Dairy'
+        },
+        {
+            name: 'Chicken',
+            standard: ['5', 'lb'],
+            have: ['5', 'lb'],
+            need: ['0', 'lb'],
+            avgCost: ['$4.95', 'lb'],
+            category: 'Dairy'
+        },
+        {
+            name: 'Topo Chico',
+            standard: ['1', 'case'],
+            have: ['.5', 'case'],
+            need: ['.5', 'case'],
+            avgCost: ['$23', 'case'],
+            category: 'Dairy'
+        },
+    ];
+
+
+
+
+
+    let addInventoryItem = {};
+    addInventoryItem.ingredient = "";
+    addInventoryItem.need = "";
+    addInventoryItem.have = "";
+    addInventoryItem.buy = "";
+    addInventoryItem.cost = "";
+
+
+    function needHaveBuyCost(){
+
+    }
+
+})();
+
+
+
+
+
+
+
+
+
+
 
 
 
