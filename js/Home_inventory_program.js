a48282f508ae7763db157fa71cdeef1edb99023a
alexander Bous <alexmbous@gmail.com>
2018-02-21 20:37:59 -0600

    refactoring old projects

---
+++ js/Home_inventory_program.js
@@ -0,0 +1,135 @@
+const inventory = [
+    {
+        name: 'butter',
+        standard: ['1', 'lb'],
+        have: ['.5', 'lb'],
+        need: ['.5', 'lb'],
+        avgCost: ['$2.25', 'lb'],
+        category: 'Dairy'
+    },
+    {
+        name: 'coffee',
+        standard: ['1', 'lb'],
+        have: ['.5', 'lb'],
+        need: ['.5', 'lb'],
+        avgCost: ['$9', 'lb'],
+        category: 'Pantry'
+    },
+    {
+        name: 'Dog Food',
+        standard: ['1', 'bag'],
+        have: ['.25', 'bag'],
+        need: ['.75', 'bag'],
+        avgCost: ['$53.99', 'bag'],
+        category: 'Pantry'
+    },
+    {
+        name: 'Baby Formula',
+        standard: ['6', 'tubs'],
+        have: ['4', 'tubs'],
+        need: ['2', 'tubs'],
+        avgCost: ['$25', 'ea'],
+        category: 'Dairy'
+    },
+    {
+        name: 'paper towels',
+        standard: ['12', 'lb'],
+        have: ['.5', 'lb'],
+        need: ['.5', 'lb'],
+        avgCost: ['$10.25', 'pack'],
+        category: 'Dairy'
+    },
+    {
+        name: 'Olive Oil',
+        standard: ['1', 'gal'],
+        have: ['1', 'gal'],
+        need: ['0', 'gal'],
+        avgCost: ['$61.00', 'gal'],
+        category: 'Dairy'
+    },
+    {
+        name: 'Chicken',
+        standard: ['5', 'lb'],
+        have: ['5', 'lb'],
+        need: ['0', 'lb'],
+        avgCost: ['$4.95', 'lb'],
+        category: 'Dairy'
+    },
+    {
+        name: 'Topo Chico',
+        standard: ['1', 'case'],
+        have: ['.5', 'case'],
+        need: ['.5', 'case'],
+        avgCost: ['$23', 'case'],
+        category: 'Dairy'
+    },
+];
+
+
+
+
+/*
+garlic
+toothpaste
+onions
+dish soap
+hand soap
+tissues
+detergent
+body wash
+shampoo
+conditioner
+baby soap
+baby lotion
+baby food pouches
+bananas
+apples
+rice
+basil
+arugula
+romaine
+jalapenos
+tomatoes
+avocados
+zucchini
+eggplant
+green tea
+milk
+eggs
+yogurt
+red wine
+white wine
+salt
+black pepper
+potatoes
+shallots
+sweet potatoes
+red vinegar
+white vinegar
+pickled hot peppers
+pickles
+mayonnaise
+sriracha
+crackers
+chicken stock
+chicken base
+beef base
+ground beef
+beef ribeye
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
